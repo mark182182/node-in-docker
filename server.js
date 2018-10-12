eb7f9ea4db331b844ce471fe86680095cd96d8c9
mark182182 <versalife11@hotmail.com>
2018-10-12 12:21:38 +0200

    add(port): for mysql connection

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const conn = mySql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 });
 
 conn.connect((err) => {
