2fea52eb37d9b2cb9f1d25915ed179bf5d3e85f4
mark182182 <versalife11@hotmail.com>
2018-10-13 17:22:48 +0200

    fix(err): show error on db error

--- server.js
+++ server.js
@@ -2,61 +2,61 @@
 
 require('dotenv').config();
 const express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser').json(),
-    path = require('path'),
-    mySql = require('mysql'),
-    PORT = process.env.PORT;
+app = express(),
+bodyParser = require('body-parser').json(),
+path = require('path'),
+mySql = require('mysql'),
+PORT = process.env.PORT;
 
 const conn = mySql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 });
 
 conn.connect((err) => {
-    if (err) {
-        console.log('Error connecting to Db');
-        return;
-    }
-    console.log(`Connection to Db established`);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(`Connection to Db established`);
 });
 
 app.use(bodyParser, express.static('assets'));
 
 app.get('/', function (req, res) {
-    res.status(200).sendFile(path.join(__dirname, 'index.html'));
+  res.status(200).sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/books', function (req, res) {
-    conn.query('SELECT * FROM books', (err, books) => {
-        if (err) {
-            console.log('Database error.');
-            return;
-        }
-        res.status(200).json(books);
-    });
+  conn.query('SELECT * FROM books', (err, books) => {
+    if (err) {
+      console.log('Database error.');
+      return;
+    }
+    res.status(200).json(books);
+  });
 });
 
 app.post('/add', function (req, res) {
-    const title = req.body.title,
-        author = req.body.author;
-    console.log(title, author);
+  const title = req.body.title,
+  author = req.body.author;
+  console.log(title, author);
 
 
-    conn.query('INSERT INTO books (title, author) VALUES (?, ?)', [title, author], (err) => {
-        if (err) {
-            console.log('Database error.');
-            return;
-        }
-        else {
-            console.log(`Inserted ${title}`);
-        }
-    });
+  conn.query('INSERT INTO books (title, author) VALUES (?, ?)', [title, author], (err) => {
+    if (err) {
+      console.log('Database error.');
+      return;
+    }
+    else {
+      console.log(`Inserted ${title}`);
+    }
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is up on port ${PORT}`);
+  console.log(`Server is up on port ${PORT}`);
 });
