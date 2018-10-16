d94c0fd186b19f7cfc19496a220a98ff0d04ac92
mark182182 <versalife11@hotmail.com>
2018-10-16 09:48:08 +0200

    add(database): aws database

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const conn = mySql.createConnection({
   host     : process.env.RDS_HOSTNAME,
   user     : process.env.RDS_USERNAME,
   password : process.env.RDS_PASSWORD,
+  database : process.env.RDS_DB_NAME,
   port     : process.env.RDS_PORT
 });
 
